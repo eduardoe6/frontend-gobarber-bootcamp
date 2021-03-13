de2f1b3380bada4926e7b885f6274d086f322805
eduardoe6 <edueduardo0699@gmail.com>
2021-03-13 00:46:27 -0300

    Crianestrutura inicial do projeto, configurando eslint e prettier, adicionando estilos globais do projeto e criando página de login da aplicação.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignIn from './pages/signIn';
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
+);
+
+export default App;
