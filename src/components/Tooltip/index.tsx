8055d9e257c254c8bba7acd107852be29e51403e
eduardoe6 <edueduardo0699@gmail.com>
2021-03-16 10:36:14 -0300

    Fazendo correção no tooltip.

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { FiAlertCircle } from 'react-icons/fi';
 import { Container } from './styles';
 
 interface TooltipProps {
@@ -10,7 +9,7 @@ interface TooltipProps {
 const Tooltip: React.FC<TooltipProps> = ({ title, className, children }) => {
   return (
     <Container className={className}>
-      <FiAlertCircle color="#c53030" size={20} />
+      {children}
       <span>{title}</span>
     </Container>
   );
